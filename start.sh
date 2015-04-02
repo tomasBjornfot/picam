8f9d69b83361381cd279b12bf28160f0421f42ae
Tomas Björnfot <tomas.bjornfoth@gmail.com>
2015-04-02 18:55:11 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-LOG=$HOME/cam/data/log.txt
+sleep 15
+LOG=/home/pi/cam/data/log.txt
+chown root /home/pi/data/*
+chmod 666 /home/pi/cam/data/*
 echo "**********"$(date)"**********" >> $LOG
 echo "starts start.sh" >> $LOG
-
+cd /home/pi/cam/code
 VAR=$(ping www.google.se -c 5 | grep "0% packet loss")
 if [ -z $VAR ]; then
 	echo "no internet connection found" >> $LOG
-	cd /home/pi/cam/code
-	sudo ./motion.py
+	./motion.py
 	sudo umount /mnt/usb
 else
 	echo "internet connection found" >> $LOG
+	echo "updates software..." >> $LOG
 	./updatecam.sh
+	echo "closeIfDisconnected.py..." >> $LOG
+	./closeIfDisconnected.py
 fi
-echo "start.sh end" >> $LOG
+echo "start.sh ending "$(date) >> $LOG
 exit 0
