f0869845cac89dd0f705287b214364ee9a05dede
Tomas Björnfot <tomas.bjornfoth@gmail.com>
2015-04-02 18:54:26 +0200

    Update motion.py

--- motion.py
+++ motion.py
@@ -2,7 +2,7 @@
 # -*- coding: iso-8859-1 -*-
 import time,os,sys,picamera,picamera.array,io,glob
 import numpy as np
-from math import sqrt
+from math import fabs
 from SimpleCV import Image, Display, Color
 
 """ ************* set date ************* """
@@ -34,20 +34,28 @@ def setCamera(cam,mode,res):
 def takeImage(cam):
     stream = picamera.array.PiRGBArray(cam)
     cam.capture(stream, format='rgb')
-    return Image( stream.array ).rotate90().rotate(180)
+    return Image( stream.array ).rotate90()
 """ ************** comapre images *************** """
-def compareImages(img1,img2):
-    diffImg = img1.scale(0.5).grayscale() - img2.scale(0.5).grayscale()
-    matrix = diffImg.getNumpy()
-    flat = matrix.flatten()
+def compareImages_old(img1,img2,diffSize):
+    img1 = img1.scale(diffSize).grayscale()
+    img2 = img2.scale(diffSize).grayscale()
+    diffImg = img1 - img2
+    flat = diffImg.getNumpy().flatten()
+    flat = np.absolute(flat)
     num_change = np.sum(flat)
-    return int(100*float(num_change)/float(len(flat)))
+    return  int(100*float(num_change)/float(len(flat)))
+
+def compareImages(img1,img2,diffSize):
+    img1 = img1.scale(diffSize)
+    img2 = img2.scale(diffSize)
+    diffImg = img1 - img2
+    return int(100.0/256.0*sum(diffImg.meanColor()))
 """ ******************* set path *********************** """
 def setPath():
     devices = glob.glob('/dev/sd?[0-9]')
     if (len(devices)==0):
 	os.system("echo 'no usb connected.' >> /home/pi/cam/data/log.txt")
-	path = '/home/pi/cam/bilder'
+	path = '/home/pi/cam/pics'
     else:
 	os.system("echo 'usb connected.' >> /home/pi/cam/data/log.txt")
 	d = devices[-1]
@@ -62,8 +70,9 @@ res = str(readSetting('cameraResolution'))
 noImages = int(readSetting('noImages'))
 sessionTime = int(readSetting('sessionTime'))
 shutDownWhenDone = str(readSetting('shutDownWhenDone'))
-wait = int(readSetting('waitTime'))
+waitTime = int(readSetting('waitTime'))
 detectLimit = int(readSetting('detectLimit'))
+waitStart = int(readSetting('waitStart'))
 
 noTimes = 0
 noIterations = 0
@@ -86,25 +95,30 @@ os.system("echo 'Exposure mode: "+str(cam.exposure_mode)+"\' >>  /home/pi/cam/da
 os.system("echo 'Detection limit: "+str(detectLimit)+"\' >>  /home/pi/cam/data/log.txt")
 os.system("echo 'Number of images: "+str(noImages)+"\' >>  /home/pi/cam/data/log.txt")
 os.system("echo 'Session time: "+str(sessionTime)+"\' >>  /home/pi/cam/data/log.txt")
+os.system("echo 'wait time: "+str(waitTime)+"\' >>  /home/pi/cam/data/log.txt")
+os.system("echo 'wait start: "+str(waitStart)+"\' >>  /home/pi/cam/data/log.txt")
 
+time.sleep(waitStart*60)
 while (noTimes < noImages and int(time.time())-timeStart<60*sessionTime):
-	time.sleep(wait)
+	time.sleep(waitTime)
 	noIterations = noIterations + 1
         newImage = takeImage(cam)
-        diff = compareImages(refImage,newImage)
+        diff = compareImages(refImage,newImage,0.1)
         info = "diff="+str(diff)+" number="+str(noTimes)+" iteration="+str(noIterations)+" time="+str(time.strftime('%H%M%S'))
         print info
 	if (diff > detectLimit):
             print "motion detected..."
             newImage.save('motion_'+date+'_'+str(noTimes)+'.png')
 	    infoFile.write(info+'\n')
-            refImage = newImage
             noTimes = noTimes + 1
+	refImage = newImage
 infoFile.close()
+
 if (shutDownWhenDone == "True"):
-	print "shutting down in 5 minutes..."
-	time.sleep(300)
+	print "shutting down in 2 minutes..."
+	time.sleep(120)
 	os.system('sudo shutdown -h now')                     
 cam.close()
 print "done..."                            
+                                         
                 
