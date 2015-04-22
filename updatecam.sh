4d61efc4f83fff4400a4910948f46fe5153b2803
Tomas Björnfot <tomas.bjornfoth@gmail.com>
2015-04-22 19:57:43 +0200

    Update updatecam.sh

--- updatecam.sh
+++ updatecam.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-LOG=$HOME/picam/data/log.txt
+LOG=/home/pi/picam/data/log.txt
 echo "updates pi.." >> $LOG
 sudo apt-get update
 sudo apt-get upgrade -y
 
 echo "gets camera scripts from github to /data/picam-master.." >> $LOG
-cd $HOME/picam/data
+cd /home/pi/picam/data
 rm -r picam-master
 wget https://github.com/tomasBjornfot/picam/archive/master.zip
 unzip master.zip
