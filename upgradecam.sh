649d65869cd1856bef97ab994182779c41af4bc5
Tomas Björnfot <tomas.bjornfoth@gmail.com>
2015-05-06 18:55:37 +0200

    Update upgradecam.sh

--- upgradecam.sh
+++ upgradecam.sh
@@ -5,10 +5,9 @@ mv updatecam.sh /home/pi/picam/code
 mv *.py /home/pi/picam/code
 mv settings.txt /home/pi/picam/data
 cd ..
+chmod 666 settings.txt
 rm -r picam-master
 
 cd /home/pi/picam/code
 chmod +x *
 
-cd /home/picam/data
-chmod 666 settings.txt
