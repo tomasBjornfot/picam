512fbe958a8b6c1269333852f8d50358730e8d12
Tomas Björnfot <tomas.bjornfoth@gmail.com>
2015-05-09 09:32:23 +0200

    Update motion.py

--- motion.py
+++ motion.py
@@ -108,7 +108,7 @@ while (noTimes < noImages and int(time.time())-timeStart<60*sessionTime):
 	noIterations = noIterations + 1
         newImage = takeImage(cam)
         diff = compareImages(refImage,newImage,0.1)
-        info = "Detection value="+str(diff)+" iteration="+str(noIterations)+" time="+str(time.strftime('%H%M%S'))
+        info = "Picture="+str(noTimes)+" DetectionValue="+str(diff)+" iteration="+str(noIterations)+" time="+str(time.strftime('%H%M%S'))
         print info
 	if (diff > detectLimit):
             print "motion detected..."
