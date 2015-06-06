4e4c5c956e49561b9a9965523cb56be18343d9d2
Tomas Björnfot <tomas.bjornfoth@gmail.com>
2015-06-06 20:26:45 +0200

    Update startCamera.py

--- startCamera.py
+++ startCamera.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-1 -*-
+from Camera import Camera
+thisCamera = Camera()
+if (thisCamera.photoMode == 'motion'):
+  thisCamera.motion()
+if (thisCamera.photoMode == 'dark'):
+  thisCamera.dark()
+if (thisCamera.photoMode == 'video'):
+  thisCamera.video()
+thisCamera.cameraClose()
 
-# camera instance...
-# read setting
-# if motion, dark or video...
 # 
